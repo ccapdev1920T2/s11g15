c77d99060b9087b292d0ca0a87fec4bf3e426bf5
AbiWnn <abigail_wan@dlsu.edu.ph>
2020-05-02 22:40:40 +0800

    Updated connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost:27017/FoodBitesDB';
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/FoodBitesDB';
 const User = require('./UserModel.js');
 const Restaurant = require('./RestaurantModel.js');
 
